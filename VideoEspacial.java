0853dbb950e8c7ae992416e843728b922e1115e3
Gabrielly <gaby.20pereiramachado@gmail.com>
2026-06-02 20:06:37 -0300

    Configurar suporte a videos via Git LFS

---
+++ VideoEspacial.java
@@ -0,0 +1,14 @@
+public class VideoEspacial {
+    
+    String titulo;
+    String formato;
+
+    public VideoEspacial(String titulo, String formato) {
+        this.titulo = titulo;
+        this.formato = formato;
+    }
+
+    public void processar() {
+        System.out.println("Processando vídeo" + titulo + "." + formato);
+    }
+}
